fdf1e1fdbf85499bfe19857e6b938f49aee63326
郭典約 <dianyueguo@gmail.com>
2023-03-21 23:57:52 +0800

    first stable version

---
+++ src/polynomial_equation_solver/PolynomialEquationSolver.java
@@ -0,0 +1,236 @@
+package polynomial_equation_solver;
+
+import java.util.ArrayList;
+
+public class PolynomialEquationSolver {
+
+	/*
+	 * This method finds all the real roots of the equation "polynomialFunction = 0"
+	 * and returns them as a double array, ordered from smallest to largest.
+	 */
+	public static double[] getRoots(PolynomialFunction polynomialFunction) throws Exception {
+//		System.out.println("getRoots: " + polynomialFunction + " = 0");
+
+		if (polynomialFunction.getDegree() == 0) {
+			if (polynomialFunction.getFunctionValue(0) == 0) {
+				throw new Exception("The equation \"0.0 = 0\" has infinitely many roots.");
+			} else {
+				return new double[0];
+			}
+		}
+
+		if (polynomialFunction.getDegree() == 1) {
+			return new double[] { -polynomialFunction.getCoefficient(0) / polynomialFunction.getCoefficient(1) };
+		}
+
+		PolynomialFunction d1f = polynomialFunction.getDerivativeFunction();
+
+		double[] d1f_roots = getRoots(d1f);
+
+//		System.out.println(Arrays.toString(d1f_roots));
+
+		if (d1f_roots.length > 0) {
+			ArrayList<Double> roots = new ArrayList<>();
+
+			double[] leftRoot = searchRootLeftward(polynomialFunction, d1f_roots[0]);
+			if (leftRoot.length != 0) {
+				roots.add(leftRoot[0]);
+			}
+
+			for (int i = 0; i < d1f_roots.length - 1; i++) {
+				double[] foundRoot = binarySearchRoot(polynomialFunction, d1f_roots[i], d1f_roots[i + 1]);
+
+				if (foundRoot.length != 0) {
+					roots.add(foundRoot[0]);
+				}
+			}
+
+			double[] rightRoot = searchRootRightward(polynomialFunction, d1f_roots[d1f_roots.length - 1]);
+			if (rightRoot.length != 0) {
+				roots.add(rightRoot[0]);
+			}
+
+			double[] returnArray = new double[roots.size()];
+			for (int i = 0; i < roots.size(); i++) {
+				returnArray[i] = roots.get(i);
+			}
+
+			return returnArray;
+		} else {
+			return searchRoot(polynomialFunction);
+		}
+	}
+
+	/*
+	 * Using newton's method to find root. f'(x) should always be positive or
+	 * negative.
+	 */
+	private static double[] searchRoot(PolynomialFunction polynomialFunction) throws Exception {
+//		System.out.println("searchRoot");
+
+		double startingPoint = 0;
+		double startingPointValue = polynomialFunction.getFunctionValue(startingPoint);
+
+		if (startingPointValue == 0) {
+			return new double[] { 0 };
+		}
+
+		PolynomialFunction d1f = polynomialFunction.getDerivativeFunction();
+
+		while (true) {
+			double nextPoint = startingPoint - startingPointValue / d1f.getFunctionValue(startingPoint); // d1f.getFunctionValue(startingPoint)
+																											// shouldn't
+																											// be zero
+			double nextPointValue = polynomialFunction.getFunctionValue(nextPoint);
+
+			if (nextPointValue == 0) {
+				return new double[] { nextPoint };
+			} else if (startingPointValue * nextPointValue < 0) {
+				if (startingPoint < nextPoint) {
+					return binarySearchRoot(polynomialFunction, startingPoint, nextPoint);
+				} else {
+					return binarySearchRoot(polynomialFunction, nextPoint, startingPoint);
+				}
+			} else {
+				startingPoint = nextPoint;
+				startingPointValue = nextPointValue;
+			}
+		}
+	}
+
+	/*
+	 * search in interval [leftBound, rightBound]
+	 */
+	private static double[] binarySearchRoot(PolynomialFunction polynomialFunction, double leftBound, double rightBound)
+			throws Exception {
+//		System.out.println("binarySearchRoot");
+
+		if (leftBound > rightBound) {
+			throw new Exception();
+		}
+
+		double leftValue = polynomialFunction.getFunctionValue(leftBound);
+
+		if (leftValue == 0) {
+			return new double[] { leftBound };
+		}
+
+		double rightValue = polynomialFunction.getFunctionValue(rightBound);
+
+		if (rightValue == 0) {
+			return new double[] { rightBound };
+		}
+
+		if (leftValue * rightValue > 0) {
+			return new double[0];
+		}
+
+		double midPoint = leftBound;
+		while (true) {
+//			System.out.println("loop");
+//			System.out.println("leftBound: " + leftBound);
+//			System.out.println("rightBound: " + rightBound);
+
+			if ((leftBound + rightBound) / 2 == midPoint) {
+				return new double[] { midPoint };
+			}
+
+			midPoint = (leftBound + rightBound) / 2;
+			double midValue = polynomialFunction.getFunctionValue(midPoint);
+
+			leftValue = polynomialFunction.getFunctionValue(leftBound);
+
+			if (midValue == 0) {
+				return new double[] { midPoint };
+			} else if (midValue * leftValue > 0) {
+				leftBound = midPoint;
+			} else {
+				rightBound = midPoint;
+			}
+		}
+
+	}
+
+	/*
+	 * search in interval [startingPoint, double_max)
+	 */
+	private static double[] searchRootRightward(PolynomialFunction polynomialFunction, double startingPoint)
+			throws Exception {
+//		System.out.println("searchRootRightward");
+
+		double startingPointValue = polynomialFunction.getFunctionValue(startingPoint);
+
+		if (startingPointValue == 0) {
+			return new double[] { startingPoint };
+		}
+
+		PolynomialFunction d1f = polynomialFunction.getDerivativeFunction();
+
+		if (startingPointValue * polynomialFunction.getCoefficient(polynomialFunction.getDegree()) > 0) {
+			return new double[0];
+		}
+
+		double nextPoint = startingPoint + 1;
+
+		do {
+			double nextPointValue = polynomialFunction.getFunctionValue(nextPoint);
+
+			if (nextPointValue == 0) {
+				return new double[] { nextPoint };
+			} else if (startingPointValue * nextPointValue < 0) {
+				return binarySearchRoot(polynomialFunction, startingPoint, nextPoint);
+			} else {
+				startingPoint = nextPoint;
+				startingPointValue = nextPointValue;
+			}
+
+			nextPoint = startingPoint - startingPointValue / d1f.getFunctionValue(startingPoint); // d1f.getFunctionValue(startingPoint)
+																									// shouldn't be zero
+		} while (true);
+	}
+
+	/*
+	 * search in interval (-double_max, startingPoint]
+	 */
+	private static double[] searchRootLeftward(PolynomialFunction polynomialFunction, double startingPoint)
+			throws Exception {
+//		System.out.println("searchRootLeftward");
+
+		double startingPointValue = polynomialFunction.getFunctionValue(startingPoint);
+
+		if (startingPointValue == 0) {
+			return new double[] { startingPoint };
+		}
+
+		PolynomialFunction d1f = polynomialFunction.getDerivativeFunction();
+
+		if (startingPointValue * polynomialFunction.getCoefficient(polynomialFunction.getDegree()) < 0) {
+			if (polynomialFunction.getDegree() % 2 == 1) {
+				return new double[0];
+			}
+		} else {
+			if (polynomialFunction.getDegree() % 2 == 0) {
+				return new double[0];
+			}
+		}
+
+		double nextPoint = startingPoint - 1;
+
+		do {
+			double nextPointValue = polynomialFunction.getFunctionValue(nextPoint);
+
+			if (nextPointValue == 0) {
+				return new double[] { nextPoint };
+			} else if (startingPointValue * nextPointValue < 0) {
+				return binarySearchRoot(polynomialFunction, nextPoint, startingPoint);
+			} else {
+				startingPoint = nextPoint;
+				startingPointValue = nextPointValue;
+			}
+
+			nextPoint = startingPoint - startingPointValue / d1f.getFunctionValue(startingPoint); // d1f.getFunctionValue(startingPoint)
+																									// shouldn't be zero
+		} while (true);
+	}
+
+}
---
+++ src/polynomial_equation_solver/PolynomialFunction.java
@@ -0,0 +1,95 @@
+package polynomial_equation_solver;
+
+import java.util.Arrays;
+
+public class PolynomialFunction {
+
+	private double[] coefficients;
+
+	/*
+	 * A polynomial "a_0 + a_1*X^1 + a_2*X^2 + ... + a_n*X^n" can be declare with
+	 * coefficients = [a_0, a_1, a_2, ... , a_n].
+	 */
+	public PolynomialFunction(double... coefficients) {
+		if (coefficients.length == 0) {
+			this.coefficients = new double[] { 0 };
+		} else {
+			for (int i = coefficients.length - 1; i >= 0; i--) {
+				if (coefficients[i] != 0 || i == 0) {
+					this.coefficients = Arrays.copyOf(coefficients, i + 1);
+					break;
+				}
+			}
+		}
+	}
+
+	public PolynomialFunction multiply(PolynomialFunction polynomialFunction) {
+		double[] coefficients = new double[this.getDegree() + polynomialFunction.getDegree() + 1];
+
+		for (int i = 0; i <= this.getDegree(); i++) {
+			for (int j = 0; j <= polynomialFunction.getDegree(); j++) {
+				coefficients[i + j] += this.getCoefficient(i) * polynomialFunction.getCoefficient(j);
+			}
+		}
+
+		return new PolynomialFunction(coefficients);
+	}
+
+	public double getFunctionValue(double x) {
+		double sum = 0;
+
+		for (int i = 0; i < coefficients.length; i++) {
+			sum += coefficients[i] * Math.pow(x, i);
+		}
+
+//		System.out.println("f(" + x + ")=" + sum);
+
+		return sum;
+	}
+
+	public PolynomialFunction getDerivativeFunction() {
+		double[] newCoefficients = new double[coefficients.length - 1];
+
+		for (int i = 1; i < coefficients.length; i++) {
+			newCoefficients[i - 1] = coefficients[i] * i;
+		}
+
+		return new PolynomialFunction(newCoefficients);
+	}
+
+	public int getDegree() {
+		return coefficients.length - 1;
+	}
+
+	public double getCoefficient(int termDegree) {
+		return coefficients[termDegree];
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < coefficients.length; i++) {
+			if (i > 0) {
+				if (coefficients[i] >= 0) {
+					sb.append(" + ");
+				} else {
+					sb.append(" - ");
+				}
+			}
+
+			if (coefficients[i] >= 0 || i == 0) {
+				sb.append(coefficients[i]);
+			} else {
+				sb.append(-coefficients[i]);
+			}
+
+			if (i > 0) {
+				sb.append("X^").append(i);
+			}
+		}
+
+		return sb.toString();
+	}
+
+}
